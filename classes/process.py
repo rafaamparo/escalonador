5f926ab19c0c9421d8e65c7f85b7ca3874b00fa6
João Lima <jg.limamarinho202@gmail.com>
2024-05-24 21:16:28 +0000

    chore: Add initial implementation of process scheduling algorithm

--- classes/process.py
+++ classes/process.py
@@ -1,7 +1,7 @@
 # crie a classe Process com o tamanho do processo, o tempo de chegada e o tempo de execução
 
 class Process:
-    def __init__(self, id, t_chegada, t_execucao_fase_1, t_disco,  t_execucao_fase_2,  tamanho, qtd_discos):
+    def __init__(self, id, t_chegada, t_execucao_fase_1, t_disco,  t_execucao_fase_2,  tamanho, qtd_discos, quantum=3):
         self.identificador = id
         self.tamanho = tamanho
         self.t_chegada = t_chegada
@@ -9,11 +9,13 @@ class Process:
         self.t_execucao_fase_2 = t_execucao_fase_2
         self.t_disco = t_disco
         self.qtd_discos = qtd_discos
+        self.contador_quantum = quantum
 
         self.t_bloqueado = 0
         self.tempo_decorrido_disco = 0
         self.tempo_executado = 0
 
+        self.novo = True
         self.bloqueado = False
         self.finalizado = False
         self.executando = False
@@ -22,7 +24,6 @@ class Process:
         self.suspenso_pronto = False
 
     def __str__(self):
-
         status = {
             'Bloqueado': self.bloqueado,
             'Finalizado': self.finalizado,
@@ -36,6 +37,25 @@ class Process:
 
         return f"Processo {self.identificador} | Chegada: {self.t_chegada} | Execução fase 1: {self.t_execucao_fase_1} u.t. | Tempo de E/S: {self.t_disco} u.t. | Execução fase 2: {self.t_execucao_fase_2} u.t. | Tamanho: {self.tamanho}mb | Qtd. Discos: {self.qtd_discos} | Status: {actual_status or 'Novo'}"
     
+    def mudar_estado(self):
+        self.bloqueado = False
+        self.finalizado = False
+        self.executando = False
+        self.pronto = False
+        self.suspenso_bloqueado = False
+        self.suspenso_pronto = False
+        self.novo = False
+        return
+
+    def admitir(self):
+        if not self.novo:
+            return
+        
+        self.mudar_estado()
+        self.pronto = True
+
+        print(f"Processo {self.identificador} foi admitido na Memória Principal")
+
     def bloquear(self):
         if not self.executando:
             return
@@ -43,12 +63,9 @@ class Process:
         if self.bloqueado:
             return
 
+        self.mudar_estado()
+
         self.bloqueado = True
-        self.finalizado = False
-        self.executando = False
-        self.pronto = False
-        self.suspenso_bloqueado = False
-        self.suspenso_pronto = False
 
         print(f"Processo {self.identificador} foi bloqueado")
 
@@ -56,12 +73,8 @@ class Process:
         if (not self.bloqueado):
             return
         
-        self.bloqueado = False
-        self.finalizado = False
-        self.executando = False
+        self.mudar_estado()
         self.pronto = True
-        self.suspenso_bloqueado = False
-        self.suspenso_pronto = False
 
         print(f"Processo {self.identificador} foi desbloqueado")
     
@@ -72,12 +85,8 @@ class Process:
         if self.finalizado:
             return
 
-        self.bloqueado = False
+        self.mudar_estado()
         self.finalizado = True
-        self.executando = False
-        self.pronto = False
-        self.suspenso_bloqueado = False
-        self.suspenso_pronto = False
 
         print(f"Processo {self.identificador} foi finalizado")
 
@@ -85,43 +94,33 @@ class Process:
         if self.suspenso_bloqueado or self.suspenso_pronto:
             return
         
-        if (not self.bloqueado) and (not self.pronto):
+        if (not self.bloqueado) and (not self.pronto) and (not self.novo):
             return
 
         if self.bloqueado:
+            self.mudar_estado()
             self.suspenso_bloqueado = True
-            self.suspenso_pronto = False
             print(f"Processo {self.identificador} foi suspenso-bloqueado")
-        else:	
-            self.suspenso_bloqueado = False
+
+        else:
+            self.mudar_estado()
             self.suspenso_pronto = True
             print(f"Processo {self.identificador} foi suspenso-pronto")
-
-        self.bloqueado = False
-        self.finalizado = False
-        self.executando = False
-        self.pronto = False
-
 
     def voltar_para_mp(self):
         if (not self.suspenso_bloqueado) and (not self.suspenso_pronto):
             return
 
         if self.suspenso_bloqueado:
+            self.mudar_estado()
             self.bloqueado = True
-            self.finalizado = False
-            self.executando = False
-            self.pronto = False
-            self.suspenso_bloqueado = False
-            self.suspenso_pronto = False
+
             print(f"Processo {self.identificador} voltou para a fila de prontos da Memória Principal")
+
         else:
-            self.bloqueado = False
-            self.finalizado = False
-            self.executando = False
+            self.mudar_estado()
             self.pronto = True
-            self.suspenso_bloqueado = False
-            self.suspenso_pronto = False
+
             print(f"Processo {self.identificador} voltou para a fila de bloqueados da Memória Principal")
 
     def executar(self):
@@ -130,46 +129,36 @@ class Process:
         
         if self.tempo_executado == self.t_execucao_fase_1:
             self.bloquear()
+            return
         
         if self.tempo_executado == self.t_execucao_fase_1 + self.t_execucao_fase_2:
             self.finalizar()
+            return
 
+        self.mudar_estado()
         self.executando = True
-
-        self.bloqueado = False
-        self.finalizado = False
-        self.pronto = False
-        self.suspenso_bloqueado = False
-        self.suspenso_pronto = False
 
         self.tempo_executado += 1
 
-        print(f"Processo {self.identificador} em execução")
+        print(f"Processo {self.identificador} em execução | Fase: { 1 if self.tempo_executado <= self.t_execucao_fase_1 else 2} | {self.tempo_executado}/{self.t_execucao_fase_1 + self.t_execucao_fase_2}")
 
     def executar_disco(self):
-        if self.suspenso_pronto or self.finalizado or self.pronto or self.executando:
+    
+        if self.t_bloqueado == self.t_disco:
+            self.desbloquear()
+
+        if self.suspenso_pronto or self.finalizado or self.pronto or self.executando or self.novo:
             return
     
         if self.bloqueado:
+            self.mudar_estado()
             self.bloqueado = True
-            self.finalizado = False
-            self.executando = False
-            self.pronto = False
-            self.suspenso_bloqueado = False
-            self.suspenso_pronto = False
+
         else:
-            self.bloqueado = False
-            self.finalizado = False
-            self.executando = False
-            self.pronto = False
+            self.mudar_estado()
             self.suspenso_bloqueado = True
-            self.suspenso_pronto = False
         
         print(f"Processo {self.identificador} está sendo executado em disco: {self.tempo_decorrido_disco}/{self.t_disco}")
 
         self.tempo_decorrido_disco += 1
-        
-        if self.t_bloqueado == self.t_disco:
-            self.desbloquear()
-
-
+    
--- main.py
+++ main.py
@@ -5,17 +5,16 @@ from classes.process import Process
 
 unidade_de_tempo = 0
 clock_delay = 1
-numero_de_processos = 0
-
+quantum = 3
+numero_de_processos = 1
+fila_de_processos = []
+fila_de_novos = []
 fila_de_execucao = []
 fila_de_prontos_0 = []
 fila_de_prontos_1 = []
 fila_de_prontos_2 = []
 fila_de_prontos_3 = []
 fila_de_bloqueados = []
-
-
-
 
 processo1 = Process(
     id=numero_de_processos,
@@ -25,16 +24,88 @@ processo1 = Process(
     t_execucao_fase_2=4,
     tamanho=800,
     qtd_discos=2,
+    contador_quantum=quantum
+)
+numero_de_processos += 1
+processo2 = Process(
+    id=numero_de_processos,
+    t_chegada=14,
+    t_execucao_fase_1=3,
+    t_disco=6,
+    t_execucao_fase_2=5,
+    tamanho=950,
+    qtd_discos=1,
+    contador_quantum=quantum
+)
+numero_de_processos += 1
+processo3 = Process(
+    id=numero_de_processos,
+    t_chegada=1,
+    t_execucao_fase_1=2,
+    t_disco=1,
+    t_execucao_fase_2=8,
+    tamanho=1024,
+    qtd_discos=3,
+    contador_quantum=quantum
+)
+numero_de_processos += 1
+processo4 = Process(
+    id=numero_de_processos,
+    t_chegada=3,
+    t_execucao_fase_1=7,
+    t_disco=5,
+    t_execucao_fase_2=3,
+    tamanho=2048,
+    qtd_discos=2,
+    contador_quantum=quantum
 )
 numero_de_processos += 1
 
+fila_de_processos = [processo1, processo2, processo3, processo4]
+
 # Nosso escalonador de processos
+print(processo1)
 while True:
     print(f"[Tempo: {unidade_de_tempo}]")
+
+    for processo in fila_de_processos:
+        if processo.t_chegada == unidade_de_tempo:
+            fila_de_novos.append(processo) # Adiciona o processo na fila de novos
+            print(f"Processo {processo.identificador} foi adicionado na fila de novos")
+
+    for processo in fila_de_novos:
+        # Verificar se o processo pode ser admitido (classe memory)
+        
+        fila_de_novos.remove(processo)
+        fila_de_prontos_0.append(processo)
+        # print(f"Processo {processo.identificador} foi adicionado na fila de prontos 0")
+        processo.admitir()
+
+    if (len(fila_de_prontos_0) > 0):
+        # Verificar se o processo pode ser executado (classe cpu)
+
+        processo = fila_de_prontos_0[0]
+        fila_de_prontos_0.remove(processo)
+        fila_de_execucao.append(processo)
+        print(f"Processo {processo.identificador} foi adicionado na fila de execução")
+    elif (len(fila_de_prontos_1) > 0):
+
+        # Verificar se o processo pode ser executado (classe cpu)
+
+        processo = fila_de_prontos_1[0]
+        fila_de_prontos_1.remove(processo)
+        fila_de_execucao.append(processo)
+        print(f"Processo {processo.identificador} foi adicionado na fila de execução")
+    elif (len(fila_de_prontos_2) > 0):
+        processo = fila_de_prontos_2[0]
+        fila_de_prontos_2.remove(processo)
+        fila_de_execucao.append(processo)
+        print(f"Processo {processo.identificador} foi adicionado na fila de execução")
+
+
+    for processo in fila_de_execucao:
+        processo.executar()
+
     unidade_de_tempo += 1
-
-    print(processo1)
-
-
     time.sleep(clock_delay)
 
